39dba4d56680b1882cbb33f7a70fd5cf832ce2b5
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-09-20 11:53:13 +0200

    fix verifyPassword function

--- utils/verify.js
+++ utils/verify.js
@@ -1,5 +1,5 @@
 export const verifyPassword = (password) => {
-    const re = new RegExp("^(?=.*[A-Z])(?=.*[a-z])(?=.*[\W_]).{8,}$")
-    return re.test(password)
+    const re = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[\W_]).{8,}$/
+    return password.match(re)
 }
 
