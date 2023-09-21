c62c4bc70aa3d481fd5b8b48111cfc05c6ced9c8
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-09-21 23:28:51 +0200

    remove unused comments

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,16 +5,6 @@ const prisma = new PrismaClient()
 
 export const authMiddleware = async (req, res, next) => {
 
-    /*
-    const {authorization} = req.headers
-
-    if (!authorization) {
-        return res.status(401).json({error: "Authorization token required"})
-    }
-
-    const token = authorization.split(" ")[1]
-     */
-
     try {
         const {token} = req.cookies
 
